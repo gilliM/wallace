da9c22e4f5d5b640cdc3205a217c68011626e68e
Gillian Milani <gillian.milani@romandie.com>
2016-05-17 19:24:31 +0200

    Change plugin button in menu

--- WilliamWallace/willian_wallace.py
+++ WilliamWallace/willian_wallace.py
@@ -21,7 +21,7 @@
  ***************************************************************************/
 """
 from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from PyQt4.QtGui import QAction, QIcon, QMenu, QToolButton
 # Initialize Qt resources from file resources.py
 import resources_rc
 # Import the code for the dialog
@@ -61,12 +61,9 @@ class WilliamWallace:
         # Create the dialog (after translation) and keep reference
         self.dlg = WilliamWallaceDialog()
 
-        # Declare instance attributes
         self.actions = []
         self.menu = self.tr(u'&William Wallace')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'WilliamWallace')
-        self.toolbar.setObjectName(u'WilliamWallace')
+
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -83,89 +80,35 @@ class WilliamWallace:
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('WilliamWallace', message)
 
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag = True,
-        add_to_menu = True,
-        add_to_toolbar = True,
-        status_tip = None,
-        whats_this = None,
-        parent = None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        self.action1 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Action 1", self.iface.mainWindow())
+        self.action2 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Action 2", self.iface.mainWindow())
+        self.action3 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Action 3", self.iface.mainWindow())
+        self.actions.append(self.action1)
+        self.actions.append(self.action2)
+        self.actions.append(self.action3)
+        self.popupMenu = QMenu(self.iface.mainWindow())
+        self.popupMenu.addAction(self.action1)
+        self.popupMenu.addAction(self.action2)
+        self.popupMenu.addAction(self.action3)
+        self.action1.triggered.connect(self.someMethod1)
+        self.action2.triggered.connect(self.someMethod2)
+        self.action3.triggered.connect(self.someMethod3)
+        self.toolButton = QToolButton()
+        self.toolButton.setMenu(self.popupMenu)
+        self.toolButton.setDefaultAction(self.action1)
+        self.toolButton.setPopupMode(QToolButton.InstantPopup)
+        self.toolbar1 = self.iface.addToolBarWidget(self.toolButton)
 
-        icon_path = ':/plugins/WilliamWallace/icon.png'
-        self.add_action(
-            icon_path,
-            text = self.tr(u'Unused text'),
-            callback = self.run,
-            parent = self.iface.mainWindow())
+    def someMethod1(self):
+        pass
+
+    def someMethod2(self):
+        pass
+
+    def someMethod3(self):
+        pass
 
 
     def unload(self):
@@ -176,7 +119,7 @@ class WilliamWallace:
                 action)
             self.iface.removeToolBarIcon(action)
         # remove the toolbar
-        del self.toolbar
+        del self.toolbar1
 
 
     def run(self):
