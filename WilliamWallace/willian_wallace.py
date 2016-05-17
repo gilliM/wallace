5507e0a56c255b9bf9ece9561ea1ce50785e6da2
Gillian Milani <gillian.milani@romandie.com>
2016-05-17 21:33:45 +0200

    Add symbology feature

--- WilliamWallace/willian_wallace.py
+++ WilliamWallace/willian_wallace.py
@@ -21,12 +21,14 @@
  ***************************************************************************/
 """
 from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon, QMenu, QToolButton
+from PyQt4.QtGui import QAction, QIcon, QMenu, QToolButton, QColor
 # Initialize Qt resources from file resources.py
 import resources_rc
 # Import the code for the dialog
 from choose_db_dialog import ChooseDbDialog
 import os.path
+import qgis
+from qgis.core import QgsDataSourceURI, QgsVectorLayer, QgsCategorizedSymbolRendererV2, QgsRendererCategoryV2, QgsSymbolV2, QgsMapLayerRegistry, QgsVectorJoinInfo
 s = QSettings()
 
 
@@ -61,6 +63,7 @@ class WilliamWallace:
 
         self.actions = []
         self.menu = self.tr(u'&William Wallace')
+        self.join = []
 
 
     # noinspection PyMethodMayBeStatic
@@ -81,7 +84,7 @@ class WilliamWallace:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
         self.action1 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Choose target DB", self.iface.mainWindow())
-        self.action2 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Action 2", self.iface.mainWindow())
+        self.action2 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Apply Symbology", self.iface.mainWindow())
         self.action3 = QAction(QIcon(":/plugins/WilliamWallace/icons/tiger.png"), u"Action 3", self.iface.mainWindow())
         self.actions.append(self.action1)
         self.actions.append(self.action2)
@@ -90,8 +93,8 @@ class WilliamWallace:
         self.popupMenu.addAction(self.action1)
         self.popupMenu.addAction(self.action2)
         self.popupMenu.addAction(self.action3)
-        self.action1.triggered.connect(self.someMethod1)
-        self.action2.triggered.connect(self.someMethod2)
+        self.action1.triggered.connect(self.ChooseTargetDB)
+        self.action2.triggered.connect(self.applySymbology)
         self.action3.triggered.connect(self.someMethod3)
         self.toolButton = QToolButton()
         self.toolButton.setMenu(self.popupMenu)
@@ -99,7 +102,7 @@ class WilliamWallace:
         self.toolButton.setPopupMode(QToolButton.InstantPopup)
         self.toolbar1 = self.iface.addToolBarWidget(self.toolButton)
 
-    def someMethod1(self):
+    def ChooseTargetDB(self):
         dialog = ChooseDbDialog()
         dialog.show()
         ok = dialog.exec_()
@@ -110,12 +113,69 @@ class WilliamWallace:
             s.setValue('WallacePlugins/connectionName', name)
         return
 
-    def someMethod2(self):
-        pass
+    def applySymbology(self):
+        classField = 'classtype'
+        self.getConnection()
+        vlayer = qgis.utils.iface.mapCanvas().currentLayer()
+        if vlayer == None:
+            return
+
+        fields = vlayer.dataProvider().fields()
+        class_loaded = False
+        for layer in QgsMapLayerRegistry.instance().mapLayers().values():
+            if  layer.name() == "class":
+                vlayerClass = layer
+                class_loaded = True
+        if not class_loaded:
+            uriSubClass = QgsDataSourceURI()
+            uriSubClass.setConnection(self.serverName, "5432", self.database, self.usr , self.pw)
+            uriSubClass.setDataSource("classification", "class", None, "", "id")
+            vlayerClass = QgsVectorLayer(uriSubClass.uri(), "class", "postgres")
+            QgsMapLayerRegistry.instance().addMapLayer(vlayerClass)
+
+        for field in fields:
+            index = vlayer.fieldNameIndex(field.name())
+            if field.name() == classField:
+                vlayer.setEditorWidgetV2(index, 'ValueRelation')
+                vlayer.setEditorWidgetV2Config(index, {'Layer': vlayerClass.id(), 'Key': 'id', 'Value': 'classname'})
+
+        useJoin = True
+        if useJoin:
+            joinObject = QgsVectorJoinInfo()
+            joinObject.joinLayerId = vlayerClass.id()
+            joinObject.joinFieldName = 'id'
+            joinObject.targetFieldName = classField
+            joinObject.memoryCache = True
+            vlayer.addJoin(joinObject)
+            self.join.append(joinObject)
+            categories = []
+            iter = vlayerClass.getFeatures()
+            for feature in iter:
+                classname = feature['classname']
+                color = QColor(feature['red'], feature['green'], feature['blue'])
+                sym = QgsSymbolV2.defaultSymbol(vlayer.geometryType())
+                sym.setColor(QColor(color))
+                category = QgsRendererCategoryV2(classname, sym, classname)
+                categories.append(category)
+            field = "class_classname"
+            renderer = QgsCategorizedSymbolRendererV2(field, categories)
+            vlayer.setRendererV2(renderer)
+
+        qgis.utils.iface.messageBar().pushMessage("Information", "Editor widget set", level = qgis.gui.QgsMessageBar.INFO, duration = 5)
+        qgis.utils.iface.setActiveLayer(vlayer)
 
     def someMethod3(self):
         pass
 
+    def getConnection(self):
+        name = s.value('WallacePlugins/connectionName')
+        self.serverName = s.value("PostgreSQL/connections/%s/host" % name)
+        self.database = s.value("PostgreSQL/connections/%s/database" % name)
+        self.port = "5432"
+        self.usr = s.value("PostgreSQL/connections/%s/username" % name)
+        self.pw = s.value("PostgreSQL/connections/%s/password" % name)
+        connectionString = "PG:dbname='%s' host='%s' port='%s' user='%s'password='%s'" % (self.database, self.serverName, self.port, self.usr, self.pw)
+        return connectionString
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
